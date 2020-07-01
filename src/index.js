805026f285538db1aa38f4cdb833db8021d87861
yunti <yunti9@icloud.com>
2020-07-01 19:28:27 +0100

    Generalise render function to work on text elements

--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@ const element = {
   type: 'h1',
   props: {
     title: 'foo',
-    children: ['Hello World'],
+    children: [
+      {
+        type: 'TEXT_ELEMENT',
+        props: {
+          nodeValue: 'Hello World',
+          children: [],
+        },
+      },
+    ],
   },
 }
 
 function render(element, container) {
   const dom =
-    typeof element === 'string'
-      ? document.createTextNode(element)
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
       : document.createElement(element.type)
-  if (element.props) {
-    element.props.children.forEach(child => render(child, dom))
+  element.props.children.forEach(child => render(child, dom))
 
-    // Set properties on DOM Element
-    const isProperty = key => key !== 'children'
-    Object.keys(element.props)
-      .filter(isProperty)
-      .forEach(name => {
-        dom[name] = element.props[name]
-      })
-  }
+  // Set properties on DOM Element
+  const isProperty = key => key !== 'children'
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
 
   container.appendChild(dom)
 }
