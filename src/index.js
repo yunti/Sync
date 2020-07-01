5c5426c13b2067497bff381cc03bf6183fc108f1
yunti <yunti9@icloud.com>
2020-07-01 18:56:10 +0100

    Add initial render function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 'use strict'
 
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: ['Hello World'],
+  },
+}
+
+function render(element, container) {
+  const dom =
+    typeof element === 'string'
+      ? document.createTextNode(element)
+      : document.createElement(element.type)
+  if (element.props) {
+    element.props.children.forEach(child => render(child, dom))
+  }
+  container.appendChild(dom)
+}
+
+const Sync = {
+  render,
+}
 const container = document.getElementById('root')
-const node = document.createElement('h1')
-node['title'] = 'foo'
-const text = document.createTextNode('')
-text['nodeValue'] = 'Hello World'
-node.appendChild(text)
-container.appendChild(node)
+// const node = document.createElement('h1')
+// node['title'] = 'foo'
+// const text = document.createTextNode('Hello World')
+// // text['nodeValue'] = 'Hello World'
+// node.appendChild(text)
+// container.appendChild(node)
+Sync.render(element, container)
