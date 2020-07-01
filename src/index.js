56f0a335af03abb819eb7092241df3fdaba77987
yunti <yunti9@icloud.com>
2020-07-01 19:51:28 +0100

    Refactor Sync to seperate module

--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
 'use strict'
 
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child =>
-        typeof child === 'object' ? child : createTextElement(child),
-      ),
-    },
-  }
-}
-
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  }
-}
-
-function render(element, container) {
-  const dom =
-    element.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type)
-  element.props.children.forEach(child => render(child, dom))
-
-  // Set properties on DOM Element
-  const isProperty = key => key !== 'children'
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name]
-    })
-
-  container.appendChild(dom)
-}
-
-const Sync = {
-  createElement,
-  render,
-}
+import Sync from './sync'
 
 /** @jsx Sync.createElement */
 const element = <h1 title="foo">Hello World</h1>
-// const textElement = Sync.createElement('TEXT_ELEMENT', {
-//   nodeValue: 'Hello World',
-// })
-// const element = Sync.createElement('h1', { title: 'foo' }, textElement)
-const container = document.getElementById('root')
 
+const container = document.getElementById('root')
 Sync.render(element, container)
--- src/sync.js
+++ src/sync.js
@@ -1,1 +1,48 @@
 'use strict'
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child),
+      ),
+    },
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+function render(element, container) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type)
+  element.props.children.forEach(child => render(child, dom))
+
+  // Set properties on DOM Element
+  const isProperty = key => key !== 'children'
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
+  container.appendChild(dom)
+}
+
+const Sync = {
+  createElement,
+  render,
+}
+
+export default Sync
