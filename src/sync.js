30efa95b7e9e72f461fb1fec0005cf9f0af63a9e
yunti <yunti9@icloud.com>
2020-07-01 20:06:21 +0100

    Add stub for workloop

--- src/sync.js
+++ src/sync.js
@@ -40,6 +40,23 @@ function render(element, container) {
   container.appendChild(dom)
 }
 
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+// requestIdleCallback(workLoop)
+
+function performUnitofWork(nextUnitOfWork) {
+  // todo
+}
+
 const Sync = {
   createElement,
   render,
